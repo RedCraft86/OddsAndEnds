5396f8cc4aa0380d4c7359c846d0e20f567b7340
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-06 16:35:54 -0400

    structure spawnpoint implementation

--- src/main/java/com/redcraft86/oddsandends/events/ServerEvents.java
+++ src/main/java/com/redcraft86/oddsandends/events/ServerEvents.java
@@ -7,10 +7,12 @@ import com.redcraft86.lanternlib.TransientFlags;
 import com.redcraft86.oddsandends.FlagKeys;
 import com.redcraft86.oddsandends.configs.CommonCfg;
 import com.redcraft86.oddsandends.OddsAndEnds;
+import com.redcraft86.oddsandends.common.*;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -21,15 +23,23 @@ import net.neoforged.neoforge.event.entity.EntityMobGriefingEvent;
 public class ServerEvents {
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    @SubscribeEvent
+    @SubscribeEvent(receiveCanceled = true)
     static void onWorldCreate(LevelEvent.CreateSpawnPosition e) {
+        LevelAccessor levelAccessor = e.getLevel();
+        if (!levelAccessor.isClientSide()) {
+            return;
+        }
+
         TransientFlags.addFlag(FlagKeys.GENERATING_WORLD);
+        if (StructureSpawnPoint.handle(levelAccessor)) {
+            e.setCanceled(true);
+        }
     }
 
     @SubscribeEvent
     static void onMobGrief(EntityMobGriefingEvent e) {
         Entity entity = e.getEntity();
-        if (!CommonCfg.isLoaded() || entity.level().isClientSide()) {
+        if (entity.level().isClientSide()) {
             return;
         }
 
