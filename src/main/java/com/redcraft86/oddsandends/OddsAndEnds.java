c4d6cce730cf1db83ff276f6a36fefc899c90a10
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-09-09 17:33:03 -0400

    registries

--- src/main/java/com/redcraft86/oddsandends/OddsAndEnds.java
+++ src/main/java/com/redcraft86/oddsandends/OddsAndEnds.java
@@ -4,10 +4,15 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import com.redcraft86.oddsandends.configs.*;
+import com.redcraft86.oddsandends.common.registries.*;
+
+import net.minecraft.world.item.CreativeModeTabs;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(OddsAndEnds.MOD_ID)
@@ -18,10 +23,21 @@ public class OddsAndEnds {
     public OddsAndEnds(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
 
         OddsAndEndsRules.registerRules();
+        ItemRegistry.ITEMS.register(modEventBus);
+        BlockRegistry.BLOCKS.register(modEventBus);
+
+        modEventBus.addListener(this::addCreative);
 
         context.registerConfig(ModConfig.Type.CLIENT, ClientCfg.SPEC);
         context.registerConfig(ModConfig.Type.COMMON, CommonCfg.SPEC);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        ItemRegistry.ITEMS.addCreative(event);
+        BlockRegistry.BLOCKS.addCreative(event);
     }
 }
--- src/main/java/com/redcraft86/oddsandends/common/registries/BlockRegistry.java
+++ src/main/java/com/redcraft86/oddsandends/common/registries/BlockRegistry.java
@@ -1,4 +1,14 @@
 package com.redcraft86.oddsandends.common.registries;
 
+import com.redcraft86.oddsandends.OddsAndEnds;
+import com.redcraft86.oddsandends.common.blocks.*;
+import com.redcraft86.lanternlib.common.registry.BlockRegister;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.CreativeModeTabs;
+
+import net.minecraftforge.registries.RegistryObject;
+
 public class BlockRegistry {
+    public static final BlockRegister BLOCKS = new BlockRegister(OddsAndEnds.MOD_ID, ItemRegistry.ITEMS);
 }
