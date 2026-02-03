85a3fd8da3c1c761ebedbaffbfd474f02911c7f7
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-02-03 16:27:10 -0500

    Fix hide experimental warning

--- src/main/java/com/redcraft86/oddsandends/mixin/common/MixinLevelSummary.java
+++ src/main/java/com/redcraft86/oddsandends/mixin/common/MixinLevelSummary.java
@@ -1,19 +1,19 @@
 package com.redcraft86.oddsandends.mixin.common;
 
 import com.redcraft86.oddsandends.configs.CommonCfg;
-import net.minecraft.world.level.storage.PrimaryLevelData;
+import net.minecraft.world.level.storage.LevelSummary;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PrimaryLevelData.class)
-public class MixinPrimaryLevelData {
-    @Inject(method = "hasConfirmedExperimentalWarning", at = @At("HEAD"), cancellable = true)
-    private void checkExperimentalWarning(CallbackInfoReturnable<Boolean> cir) {
+@Mixin(value = LevelSummary.class)
+public class MixinLevelSummary {
+    @Inject(method = "isExperimental", at = @At(value = "RETURN"), cancellable = true)
+    private void isExperimental(CallbackInfoReturnable<Boolean> cir) {
         if (CommonCfg.HIDE_EXPERIMENTAL_WARNING.get()) {
-            cir.setReturnValue(true);
+            cir.setReturnValue(false);
         }
     }
 }
