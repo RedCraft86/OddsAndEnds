7d7b6f0b2b4c40b45591d248a6e8b787f0afd3dc
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-05-26 11:58:53 -0400

    Fix crash with TemptGoal.calmDown not shadowing

--- src/main/java/com/redcraft86/oddsandends/mixin/common/MixinTemptGoal.java
+++ src/main/java/com/redcraft86/oddsandends/mixin/common/MixinTemptGoal.java
@@ -4,19 +4,16 @@ import com.redcraft86.oddsandends.configs.CommonCfg;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TemptGoal.class)
 public class MixinTemptGoal {
-    @Shadow private int calmDown;
-
     @Inject(method = "stop", at = @At("TAIL"))
     private void one_stop(CallbackInfo ci) {
         if (CommonCfg.NO_TEMPT_COOLDOWN.get()) {
-            calmDown = 0;
+            ((TemptGoal)(Object)this).calmDown = 0;
         }
     }
 }
